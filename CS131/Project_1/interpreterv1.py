e6280c3e5e299b145735f9689857dfa6950141f8
Warren Kim <blankmkb@gmail.com>
2023-04-28 10:36:02 -0700

    CS131: added to interpreter

--- CS131/Project_1/interpreterv1.py
+++ CS131/Project_1/interpreterv1.py
@@ -1,4 +1,4 @@
-from intbase import InterpreterBase
+from intbase import InterpreterBase as base
 from bparser import BParser
 
 class Method:
@@ -15,7 +15,7 @@ class Field:
     def __init__(self, name = "", value = None):
         self.name = name
         self.value = value
-        self.type = bool if value == "true" or value == "false" else type(eval(value))
+        self.type = bool if value == base.TRUE_DEF or value == base.FALSE_DEF else type(eval(value))
 
     def change_value(self, new_value):
         self.value = new_value
@@ -35,7 +35,7 @@ class Class:
 
 
 
-class Interpreter(InterpreterBase):
+class Interpreter(base):
     def __init__(self, console_output = True, inp = None, wtfisthis = None):
         super().__init__(console_output, inp)
         self.classes = { }
@@ -50,23 +50,44 @@ class Interpreter(InterpreterBase):
             if type(token) is list:
                 self.p_input(token, class_name, method_name)
 
-            # new class
+            # class
             if token == self.CLASS_DEF:
                 class_name = tokens[i + 1]
                 self.classes[class_name] = Class(class_name)
 
-            # new field
+            # method
+            elif token == self.METHOD_DEF:
+                method_name = tokens[i + 1]
+                method_args = tokens[i + 2]
+                self.classes[class_name].add_method(method_name, method_args)
+
+            # field
             elif token == self.FIELD_DEF:
                 field_name = tokens[i + 1]
                 field_value = tokens[i + 2]
                 self.classes[class_name].add_field(field_name, field_value)
-                
-            # new method
-            elif token == self.METHOD_DEF:
-                method_name = tokens[i + 1]
-                method_args = tokens[i + 2]
 
-                self.classes[class_name].add_method(method_name, method_args)
+            # begin
+            elif token == self.BEGIN_DEF:
+                # for each statement in the begin block, recurse to parse the statement
+                for statement in tokens[i + 1]:
+                    self.p_input(statement, class_name, method_name)
+
+            # null
+            elif token == self.SET_DEF:
+                pass
+
+            # null
+            elif token == self.NEW_DEF:
+                pass
+
+            # null
+            elif token == self.IF_DEF:
+                pass
+
+            # null
+            elif token == self.WHILE_DEF:
+                pass
 
             # new print statement
             elif token == self.PRINT_DEF:
@@ -78,8 +99,11 @@ def main():
     program_source = ['(class main',
                       '(field x true)',
                       ' (method main ()',
+                      '(begin',
                       '   (print (call me hello))',
+                      '(print (something else))',
                       ' ) # end of method',
+                      ')',
                       ' (method other (one)',
                       '  (print "other")',
                       ')',
@@ -94,8 +118,6 @@ def main():
     _, program = BParser.parse(program_source)
 
     x = Interpreter(inp = program)
-    print(x.inp)
-    print()
     x.p_input()
 
     print(f"class(es): {x.classes}")
