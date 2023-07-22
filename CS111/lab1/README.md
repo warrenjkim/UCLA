29863d21de11b83916b64410c74e12a77f875a54
Warren Kim <blankmkb@gmail.com>
2023-07-21 21:49:52 -0700

    CS111: finished lab 1

--- CS111/lab1/README.md
+++ CS111/lab1/README.md
@@ -1,18 +1,54 @@
-## UID: 123456789
+## UID: 305600694
 (IMPORTANT: Only replace the above numbers with your true UID, do not modify spacing and newlines, otherwise your tarfile might not be created correctly)
 
 ## Pipe Up
 
-One sentence description
+A program that recreates the pipe (|) operator in the shell.
 
 ## Building
 
-Explain briefly how to build your program
+To build the program, in the lab1 directory, do :
+``
+make
+``
+or in the lab1 directory, do:
+``
+gcc pipe.c -o pipe
+``
 
 ## Running
 
 Show an example run of your program, using at least two additional arguments, and what to expect
+Example 1:
+``
+./pipe ls cat
+``
+should return:
+``
+Makefile
+README.md
+pipe
+pipe.c
+test_lab1.py
+``
+
+Example 2:
+``
+./pipe ls cat wc
+``
+should return:
+``
+       5       5      44
+``
+
 
 ## Cleaning up
 
-Explain briefly how to clean up all binary files
+To remove the program, in the lab1 directory, do:
+``
+make clean
+``
+or in the lab1 directory, do:
+``
+rm ./pipe
+``
--- CS111/lab1/pipe.c
+++ CS111/lab1/pipe.c
@@ -6,41 +6,34 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-static void error_and_exit(const char * type) {
-  char reason[11];
-  sprintf(reason, "%s failed", type);
-  perror(reason);
-  exit(EXIT_FAILURE);
-}
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {                             // check for valid argc
-    perror("invalid arguments");
+  if (argc < 2)                               // check for valid argc
     exit(EINVAL);
-  }
   
   
   if (argc == 2) {                            // only one command <==> no pipe needed => execute command
     execlp(argv[1], argv[1], (char *) NULL);
-    exit(EXIT_FAILURE);
+    exit(errno);
   }
 
-  int   num_pipes = argc - 2;   // number of pipes needed:      argc - 2
-  int   num_pids  = argc - 1;   // number of child pids needed: argc - 1
+  int   num_pipes = argc - 2;  // number of pipes needed:      argc - 2
+  int   num_pids  = argc - 1;  // number of child pids needed: argc - 1
 
+  int   parent    = 0;         // determine if we are the parent for clean up
+  
   int   pipefds[num_pipes][2]; // [i][0]: read, [i][1]: write
   pid_t child_pids[num_pids];  // child pids for wait
   int   wstatuses[num_pids];   // wait statuses for child i
-  
 
   for (int i = 0; i < num_pids; i++) {                      // loop for each process
     if (pipe(pipefds[i]) < 0)                               // check for pipe failure
-      error_and_exit("pipe");
+      exit(errno);
 
     child_pids[i] = fork();                                 // fork process
 
     if (child_pids[i] < 0)                                  // check for fork failure
-      error_and_exit("fork");
+      exit(errno);                   
 
     if (child_pids[i] == 0) {                               // child process => route pipes and execute commands
       if (i == 0) {                                         // first command
@@ -53,33 +46,41 @@ int main(int argc, char *argv[]) {
 	dup2(pipefds[i - 1][0], STDIN_FILENO);              // reroute stdin to read end of previous pipe
 	close(pipefds[i - 1][0]);                           // close old read end of previous pipe
 
+	close(pipefds[i][0]);
+
 	if (i == num_pipes) {
+	  close(pipefds[i][1]);
 	  execlp(argv[i + 1], argv[i + 1], (char *) NULL);  // execute command
-	  error_and_exit("execlp");                         // execlp failed
+	  exit(errno);                                      // execlp failed
 	}
-	close(pipefds[i][0]);                               // close unused read end of pipe
+
 	dup2(pipefds[i][1], STDOUT_FILENO);                 // reroute stdout to write end of pipe
 	close(pipefds[i][1]);                               // close old write end of pipe
       }
 
       execlp(argv[i + 1], argv[i + 1], (char *) NULL);      // execute command
-      error_and_exit("execlp");                             // check for execlp failure
+      exit(errno);                                          // execlp failed
     }
-    else
-      close(pipefds[i][1]);                                 // close write end of pipe 
-  }
-    
-  for (int i = 0; i < num_pids; i++) {                                     // for each child pid
-    waitpid(child_pids[i], &wstatuses[i], 0);                              // wait
-    if (WIFEXITED(wstatuses[i]) || WIFSIGNALED(wstatuses[i])) {            // check for 0 wait status
-      if (WEXITSTATUS(wstatuses[i]) != 0) {                                // error exit code
-	char error_code[15];                                               // error code string
-	sprintf(error_code, "error code: %d", WEXITSTATUS(wstatuses[i]));  // stringify exit message
-	perror(error_code);                                                // error
-	exit(WEXITSTATUS(wstatuses[i]));                                   // exit with error
-      }
+    else {
+      close(pipefds[i][1]);                                 // close write end of pipe
+      parent = 1;                                           // we are the parent
     }
   }
 
+  if (parent) {
+    for (int i = 0; i < num_pids; i++) {
+      close(pipefds[i][0]);
+      pid_t w;
+
+      w = waitpid(child_pids[i], &wstatuses[i], 0);                          // wait
+      if (w < 0)
+	exit(errno);
+      if (WIFEXITED(wstatuses[i]) || WIFSIGNALED(wstatuses[i]))              // check for 0 wait status
+	if (WEXITSTATUS(wstatuses[i]) != 0)                                  // error exit code
+	  exit(WEXITSTATUS(wstatuses[i]));                                   // exit with error
+
+    }
+  }
+    
   return 0;
 }
