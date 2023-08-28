6f97b499e51a1d1ec4dd78294c0bdc588ec845f9
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2023-08-28 16:12:21 -0700

    CS35L: removed project

--- CS35L/CS35L_Project/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Component() {
-    return (
-        <div className="p-4">
-            <p>&copy; 2023 Talha Abadin, Steven Bash, Curtis Chou, Lucas Kelley, & Warren Kim</p>
-        </div>
-    )
-}
--- CS35L/CS35L_Project/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git log | grep warren > logs.txt
-git log | grep gibbs >> logs.txt
-git log | grep stvnbash >> logs.txt
-git log | grep kelley >> logs.txt
-git log | grep curtis >> logs.txt
